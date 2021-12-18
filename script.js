961ec8f36114087df680f891a4fd791a098c70f2
solar <spidervvv@mail.ru>
2021-12-18 23:21:21 +0300

    add displaying a map marker

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ if (navigator.geolocation) {
     function (position) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(`https://www.google.ru/maps/@${latitude},${longitude}`);
+      //   console.log(`https://www.google.ru/maps/@${latitude},${longitude}`);
 
       const coords = [latitude, longitude];
 
@@ -27,10 +27,23 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      //click on map
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position!');
